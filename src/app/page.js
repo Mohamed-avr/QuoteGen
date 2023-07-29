dbd14caa43f9360cdec191eb721d860cd054a486
mohamed <mouh4945@gmail.com>
2023-07-29 13:54:01 +0100

    UPDT

--- src/app/page.js
+++ src/app/page.js
@@ -57,8 +57,8 @@ export default function Home() {
           </div>
           <div className='flex justify-between items-center sm:flex-row flex-col  flex-nowrap  '>
 
-            <div className='flex justify-center flex-col space-y-2 sm:w-[20%] w-full'>
-              <h5 className='text-3xl mt-8 font-bold '>{randomQuote.author || "Anonymous"}</h5>
+            <div className='flex justify-center flex-col space-y-2 sm:w-[35%] w-full'>
+              <h5 className='sm:text-3xl mt-8 font-bold text-xl  '>{randomQuote.author || "Anonymous"}</h5>
               <h5 className=' font-medium pl-1'> Lived</h5>
               <h5 className=' font-medium pl-1'> 1881/1955</h5>
               <h5 className=' font-medium pl-1'> new jersey</h5>
@@ -66,15 +66,15 @@ export default function Home() {
               {/*  */}
             </div>
 
-            <div className=' flex sm:justify-evenly justify-between items-center flex-row sm:w-[80%]'>
-              <div className='sm:w-[50%] sm:pt-16 sm:mt-0 mt-6'>
-                <p className='font-bold'> Pablo Ruiz Picasso (25 October 1881 – 8 April 1973) was a Spanish painter, sculptor, printmaker, ceramicist and theatre designer who spent most of his adult life in France. One of the most influential artists of the 20th century, he is known for co-founding the Cubist movement, the invention ...</p>
+            <div className=' flex sm:justify-evenly  justify-between items-center  flex-row sm:w-[90%] w-full '>
+              <div className='sm:w-[50%] w-full sm:pt-16 sm:mt-0 mt-6'>
+                <p className='font-bold '> Pablo Ruiz Picasso (25 October 1881 – 8 April 1973) was a Spanish painter, sculptor, printmaker, ceramicist and theatre designer who spent most of his adult life in France. One of the most influential artists of the 20th century, he is known for co-founding the Cubist movement, the invention ...</p>
               </div>
 
-              <div className='mt-10'>
+              <div className='mt-10 sm:flex-none sm:relative fixed right-10  sm:bottom-0 bottom-[10rem] '>
                 <button
                   onClick={ranIndexHandling}
-                  className=' bg-neutral-300 sm:flex   top-[480px] right-5  border-2  sm:w-20 sm:h-20 w-16 h-16 pt-1 font-bold flex justify-center items-center  hover:delay-100  rounded-full'> Gen
+                  className=' bg-neutral-300 sm:flex top-[480px] right-5 border-2 sm:w-20 sm:h-20 w-16 h-16 pt-1 font-bold flex justify-center items-center  hover:delay-100  rounded-full'> Gen
                 </button>
               </div> </div>
 
